a83890c09cb705c6565d063d7210571c6f21fb71
Radifan Mardhi Prana <43719706+radifanmp@users.noreply.github.com>
2021-02-23 12:25:59 +0700

    Looping for

---
+++ looping.js
@@ -0,0 +1,28 @@
+var cars = ["BMW", "Mercedes-Benz", "Tesla", "Toyota", "Ford"];
+var i = 0;
+var car = ""
+
+// for(var i=1; i <=5; i++){
+//     document.write("<p>This Number " +i+ "</p>")
+// }
+
+for(var i=0; i<=cars.length; i++){
+    document.write(cars[i]+ "<br><br>")
+}
+
+// while(cars[i]){
+//     car += cars[i] + "<br><br>";
+//     i++;
+// }
+
+// do{
+//     car += cars[i] + "<br><br>";
+//     i++;
+// }while(cars[i])
+
+// document.write(car)
+
+// while(i < 5){
+//     document.write("<p>The Number is " +i +"</p>")
+//     i++
+// }
