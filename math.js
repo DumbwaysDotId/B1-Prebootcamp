152f4261cde1858393eff1d1cb60aa273b77560e
fadhil <fadhildarma13@gmail.com>
2021-02-25 11:49:12 +0700

    math

---
+++ math.js
@@ -0,0 +1,34 @@
+const now = new Date();
+const date = now.toString();
+
+console.log(date);
+
+const nilaiRandom = Math.random() * 1000;
+
+console.log(Math.ceil(nilaiRandom));
+
+const nilaiMax = Math.max(1, 2, 3, 4, 5, 6);
+
+console.log(nilaiMax);
+
+const name = "1234567";
+const fullName = "Fadhil Darma Putera Zagoto";
+const stringArray = Array.from(name);
+
+console.log(stringArray);
+
+const arrayFullname = fullName.split(" ");
+
+const firstName = arrayFullname.map((name, index) => {
+  if (index !== arrayFullname.length - 1) {
+    return {
+      firstName: name,
+    };
+  }
+})[0];
+
+const lastName = arrayFullname.filter(
+  (name, index) => index === arrayFullname.length - 1
+)[0];
+
+console.log({ firstName, lastName });
